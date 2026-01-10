0b70a98371b2fc58be008d661a1b1c1ef0163c4a
Javid Ahmed <javidahmed@icloud.com>
2026-01-10 16:31:02 +0000

    Add constants for steganography application

---
+++ python_steganographer/constants.py
@@ -0,0 +1,11 @@
+"""Constants for the steganographer application."""
+
+import numpy as np
+
+MAX_BITS_PER_PIXEL = 8
+NUM_BITS = MAX_BITS_PER_PIXEL - 1
+BIT_MAP = np.array([2 ** (NUM_BITS - 1 - i) for i in range(NUM_BITS)])
+
+DEFAULT_BLOCK_SIZE = 8
+DEFAULT_DCT_COEFFICIENT = 3  # Which DCT coefficient to modify (1-indexed, avoid DC component)
+QUANTIZATION_FACTOR = 10  # Quantization factor for DCT coefficients
--- python_steganographer/helpers.py
+++ python_steganographer/helpers.py
@@ -4,9 +4,7 @@ import base64
 
 import numpy as np
 
-MAX_BITS_PER_PIXEL = 8
-NUM_BITS = MAX_BITS_PER_PIXEL - 1
-BIT_MAP = np.array([2 ** (NUM_BITS - 1 - i) for i in range(NUM_BITS)])
+from python_steganographer.constants import BIT_MAP, NUM_BITS
 
 
 def bytes_to_str(msg: bytes) -> str:
